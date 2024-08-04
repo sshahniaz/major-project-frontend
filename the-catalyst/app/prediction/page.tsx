89fb8fce90cb70d200308c4268600a5728934069
sshahniaz <100251uk@saeinstitute.edu>
2024-08-05 00:30:43 +0100

    added image download

--- the-catalyst/app/prediction/page.tsx
+++ the-catalyst/app/prediction/page.tsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, PointElement, LineElement, Legend, Title } from 'chart.js';
 import { ProductType, OutletSize, OutletType, LocationType, SalesData, Top10ProductTypesSalesInterface } from '@/types/types';// Assuming 'types' folder for interfaces
 import { Bar } from 'react-chartjs-2';
-
 ChartJS.register(
   BarElement,
   CategoryScale,
@@ -36,7 +35,11 @@ const Prediciton = () => {
   const [error, setError] = useState<any>(null);
   const [ baselineData, setBaselineData ] = useState<SalesData | null>(null); // Stores baseline data
   const [predictedResult, setPredictedResult] = useState<SalesData | null>(null); // Stores predicted result
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [ isDarkMode, setIsDarkMode ] = useState(false);
+  const [ isDownloadEnabled, setIsDownloadEnabled ] = useState(false);
+
+
+
   useEffect(() => {
     const fetchBaselineData = async () => {
       try {
@@ -79,7 +82,7 @@ const Prediciton = () => {
       
      
       setPredictedResult(response.data);
-      
+      setIsDownloadEnabled(true)
       setFormData(formData); // Update formData with predicted result
       
 
@@ -89,6 +92,61 @@ const Prediciton = () => {
     }
     setIsLoading(false);
   }
+
+  const handleDownloadCSV = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get('https://sea-turtle-app-vktl8.ondigitalocean.app/downloadPredicted', {
+        responseType: 'blob',
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'predicted_sales.csv');
+      document.body.appendChild(link);
+      link.click();
+      setIsDownloadEnabled(false);
+    } catch (error: any) {
+      console.error('Error downloading CSV:', error);
+      setError(error);
+    }
+    setIsLoading(false);
+  }
+
+ const handleDownloadChart = async () => {
+    if (!baselineData) {
+      console.error('No chart data available to download.');
+      return;
+    }
+
+    const chartContainer: any = document.getElementById('chart-container'); // Assuming an element with this ID wraps your chart
+
+    if (!chartContainer) {
+    console.error('Chart container element not found.');
+    return;
+  }
+
+  const canvas = chartContainer.querySelector('canvas');
+
+  if (!canvas) {
+    console.error('Canvas element not found inside chart container.');
+    return;
+  }
+
+  try {
+    const dataURL = canvas.toDataURL('image/png'); // Get the image URL
+    const link = document.createElement('a'); // Create a download link
+    link.href = dataURL; // Set the URL to the image data
+    link.download = 'chart.png'; // Set the name for the downloaded file
+    document.body.appendChild(link); // Append link to the body (required for Firefox)
+    link.click(); // Trigger the download
+    document.body.removeChild(link); // Clean up the link element
+  } catch (error) {
+    console.error('Error downloading chart:', error);
+  }
+  };
 
   const chartOptions = {
     scales: {
@@ -207,12 +265,12 @@ const Prediciton = () => {
 
         <h2 className='text-gray-800 dark:text-white text-6xl pt-8'>Predictions</h2>
        
-        <div className="flex flex-col md:flex-row justify-between items-center gap-4 mt-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4 mt-4 ">
           <div className="bg-white shadow-sm rounded-lg overflow-hidden dark:bg-gray-800">  {/* Form card with dark mode switch */}
             <div className="px-4 py-5 sm:p-6">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-wrap -mb-3">
-              <label htmlFor="file" className="text-lg font-medium text-gray-800 dark:text-white mb-1 w-full sm:w-1/2">
+              <label htmlFor="file" className="text-xl font-semibold text-gray-800 dark:text-white mb-1 w-full sm:w-1/2">
                 Upload File
                   </label>
                   {/* File input */}
@@ -227,13 +285,48 @@ const Prediciton = () => {
             </div>
             <button
               type="submit"
-              className="inline-flex items-center px-4 py-2 mt-4 bg-indigo-500 rounded-md font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="inline-flex text-xl md:text-2xl items-center px-4 py-2 mt-4 bg-indigo-500 rounded-md font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Predict
             </button>
           </form>
         </div>
-      </div>
+          </div>
+          {/* Download chart */}
+           <div className="bg-white shadow-sm rounded-lg overflow-hidden dark:bg-gray-800">
+              <div className="px-4 py-5 sm:p-6">
+                <h3 className="text-gray-700 dark:text-gray-200 text-xl md:text-2xl font-semibold">Download Chart</h3>
+                <button
+                  id="download-chart-button"
+                  onClick={handleDownloadChart}
+                  className={`inline-flex items-center px-4 py-2 mt-4 bg-indigo-500 hover:bg-indigo-700 rounded-md font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+                  
+                >
+                  Download PNG
+                </button>
+              </div>
+            </div>
+
+          {/*Explaination of download button and button*/}
+          <div className="bg-white shadow-sm rounded-lg overflow-hidden dark:bg-gray-800">  {/* Download card */}
+            <div className="px-4 py-5 sm:p-6">
+              <h3 className="text-gray-800 dark:text-white text-xl md:text-2xl font-semibold">Download Predicted Data</h3>
+              <p className="text-gray-800 dark:text-white text-lg md:text-xl mt-5">Download the predicted sales data in CSV format. <br/>
+                It's only available after making a prediction. <br/> And for security reasons we recommend downloading the file immediately after prediction. <br/>
+              </p>
+              <button
+            onClick={handleDownloadCSV}
+            className={`inline-flex items-center px-4 py-2 mt-4 ${
+              isDownloadEnabled ? 'bg-indigo-500 hover:bg-indigo-700' : 'bg-gray-500 cursor-not-allowed'
+            } rounded-md font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+            disabled={!isDownloadEnabled}
+          >
+            Download CSV
+          </button>
+            </div>
+          </div>
+
+           
 
       {isLoading && <p className="text-center text-gray-500 dark:text-gray-300 text-lg">Loading...</p>}
       {error && <p className="text-center text-red-500 text-lg">Error: {error.message}</p>}
@@ -243,16 +336,17 @@ const Prediciton = () => {
           <div className="mt-8">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="bg-white shadow-sm rounded-lg overflow-hidden dark:bg-gray-800">  {/* Top 10 Products Chart card */}
-                <div className="px-4 py-5 sm:p-6">
+                <div id='chart-container' className="px-4 py-5 sm:p-6">
                    <h3 className='"text-gray-700 dark:text-gray-200 text-xl md:text-2xl font-semibold'>Top 10 Products (Average Sales)</h3>
-                   <Bar data={generateChartData(baselineData?.top10ProductTypesSales || [], predictedResult?.top10ProductTypesSales || [], 'Top 10 Sales')} options={chartOptions} />
+                  <Bar data={generateChartData(baselineData?.top10ProductTypesSales || [], predictedResult?.top10ProductTypesSales || [], 'Top 10 Sales')} options={chartOptions} />
+                  
                 </div>
               </div>
 
               <div className="bg-white shadow-sm rounded-lg overflow-hidden dark:bg-gray-800">  {/* Location Types Chart card */}
                 <div className="px-4 py-5 sm:p-6">
                   <h3 className='"text-gray-700 dark:text-gray-200 text-xl md:text-2xl font-semibold'>Location Types (Average Sales)</h3>
-                  <Bar data={generateChartData(baselineData?.locationType || [], predictedResult?.top10ProductTypesSales || [], 'Location Tier Average Sales')} options={chartOptions} />
+                  <Bar data={generateChartData(baselineData?.locationType || [], predictedResult?.locationType || [], 'Location Tier Average Sales')} options={chartOptions} />
                </div>
               </div>
 
